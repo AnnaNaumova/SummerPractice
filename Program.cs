6b8d4b2ea064d663a436db33f6e7336afdfa7d0f
AnnaNaumova <Anuta3.97@mail.ru>
2016-06-21 19:46:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("У меня всё же не получилось написать свои программы. После сегодняшней практики эта строка - то единственное,что я смогла сделать!  ");
+            Console.ReadKey();
+        }
+    }
+}
