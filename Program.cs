ab6156c2804fdfaf71ed954bc4c52e94c18082fe
AnnaNaumova <Anuta3.97@mail.ru>
2016-06-23 19:03:54 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,7 +10,26 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("У меня всё же не получилось написать свои программы. После сегодняшней практики эта строка - то единственное,что я смогла сделать!  ");
+            int n;
+            Random x = new Random();
+            Console.WriteLine("Введите длину массива:");
+            n = Convert.ToInt32(Console.ReadLine());
+            double[] a = new double[n];
+            double[] b = new double[n];
+            double k = a[0];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(i + " элемент массива а = " + (a[i] = x.Next())); //случайное задание элементов массива
+                //Console.WriteLine("Введите "+i+" элемент:"); //ручной ввод элементов массива
+               // a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                k = k + a[i];
+                b[i] = a[i] / (1 + Math.Pow(k, 2));
+                Console.WriteLine(i+" элемент массива b = "+b[i]);
+            }
+
             Console.ReadKey();
         }
     }
